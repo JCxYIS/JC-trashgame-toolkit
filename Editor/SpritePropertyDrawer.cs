012c5718e52afd389f8ec826cefbd2247cc82ac2
JCxYIS <jsn811@gmail.com>
2021-12-19 21:36:03 +0800

    update sprire property drawer

--- Editor/SpritePropertyDrawer.cs
+++ Editor/SpritePropertyDrawer.cs
@@ -3,47 +3,45 @@ using UnityEditor;
 
 /// <summary>
 /// Preview sprite in inspector
-/// Code from https://pastebin.com/Y1R7eSA0
+/// Code from https://github.com/BartInTheField/Unity-SpritePropertyDrawer.git
 /// </summary>
 [CustomPropertyDrawer(typeof(Sprite))]
 public class SpritePropertyDrawer : PropertyDrawer
 {
+    private const float textSize = 70;
+
     public override float GetPropertyHeight(SerializedProperty property, GUIContent labelN)
     {
         if (property.objectReferenceValue != null)
         {
-            return _texSize;
+            return textSize;
         }
         else
         {
             return base.GetPropertyHeight(property, labelN);
         }
     }
-
-    private const float _texSize = 70;
-
-    
-
+ 
     public override void OnGUI(Rect position, SerializedProperty prop, GUIContent label)
     {
         EditorGUI.BeginProperty(position, label, prop);
-
+ 
         if (prop.objectReferenceValue != null)
         {
             position.width = EditorGUIUtility.labelWidth;
             GUI.Label(position, prop.displayName);
-
+ 
             position.x += position.width;
-            position.width = _texSize;
-            position.height = _texSize;
-
+            position.width = textSize;
+            position.height = textSize;
+ 
             prop.objectReferenceValue = EditorGUI.ObjectField(position, prop.objectReferenceValue, typeof(Sprite), false);
         }
         else
         {
             EditorGUI.PropertyField(position, prop, true);
         }
-
+ 
         EditorGUI.EndProperty();
     }
 }
